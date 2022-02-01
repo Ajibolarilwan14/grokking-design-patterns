d7abff42d9d8a6acc0500b166d7d0e63a3517438
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2022-02-01 13:27:19 +0100

    creating abstract class for Aircraft

---
+++ creational_pattern/builder_pattern.java
@@ -0,0 +1,11 @@
+public abstract class AircraftBuilder {
+    public void buildEngine() {
+
+    }
+}
+
+// public class builder_pattern {
+//     public static void main(String[] args) {
+//         System.out.println("Hello World!");
+//     }
+// }
