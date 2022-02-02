eca34379e4f0ccc9ea84b563f1917ed46844b141
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2022-02-02 13:14:55 +0100

    added the client class

--- creational_pattern/builder_pattern.java
+++ creational_pattern/builder_pattern.java
@@ -99,6 +99,18 @@ public class builder_pattern {
                 aircraftBuilder.buildBathrooms();
         }
     }
+
+    public class Client {
+
+        public void main() {
+    
+            F16Builder f16Builder = new F16Builder();
+            Director director = new Director(f16Builder);
+            director.construct(false);
+    
+            IAircraft f16 = f16Builder.getResult();
+        }
+    }
     
 }
 
