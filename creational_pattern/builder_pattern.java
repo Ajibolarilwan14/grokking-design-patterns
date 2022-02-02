41d7aad9846ce43eb983b3831c61c6b8ff95a42b
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2022-02-02 13:07:46 +0100

    added the director class

--- creational_pattern/builder_pattern.java
+++ creational_pattern/builder_pattern.java
@@ -81,6 +81,24 @@ public class builder_pattern {
         }
         
     }
+
+    public class Director {
+
+        AircraftBuilder aircraftBuilder;
+    
+        public Director(AircraftBuilder aircraftBuilder) {
+            this.aircraftBuilder = aircraftBuilder;
+        }
+    
+        public void construct(boolean isPassenger) {
+            aircraftBuilder.buildCockpit();
+            aircraftBuilder.buildEngine();
+            aircraftBuilder.buildWings();
+    
+            if (isPassenger)
+                aircraftBuilder.buildBathrooms();
+        }
+    }
     
 }
 
