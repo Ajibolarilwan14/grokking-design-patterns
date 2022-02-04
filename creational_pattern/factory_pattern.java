04fdba93a13f9cc5e6faa43a2feffdee0a7ee039
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2022-02-04 11:33:12 +0100

    added a simple factory pattern

--- creational_pattern/factory_pattern.java
+++ creational_pattern/factory_pattern.java
@@ -27,3 +27,18 @@ public class Client {
         f16.fly();
     }
 }
+
+public class F16SimpleFactory {
+
+    public F16 makeF16(String variant) {
+
+        switch (variant) {
+        case "A":
+            return new F16A();
+        case "B":
+            return new F16B();
+        default:
+            return new F16();
+        }
+    }
+}
