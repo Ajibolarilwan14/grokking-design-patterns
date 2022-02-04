5f18ee85ac1c9a2e1434da7e9b9baa140c72df49
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2022-02-04 11:57:56 +0100

    code to an interface and not an implementation

--- creational_pattern/abstractFactory_pattern.java
+++ creational_pattern/abstractFactory_pattern.java
@@ -1,13 +1,26 @@
-public class abstractFactory_pattern {
-    public void main() {
-        F16Cockpit f16Cockpit = new F16Cockpit();
-        F16Engine f16Engine = new F16Engine();
-        F16Wings f16Wings = new F16Wings();
+// public class abstractFactory_pattern {
+//     public void main() {
+//         F16Cockpit f16Cockpit = new F16Cockpit();
+//         F16Engine f16Engine = new F16Engine();
+//         F16Wings f16Wings = new F16Wings();
     
-        List<F16Engine> engines = new ArrayList<>();
-        engines.add(f16Engine);
-        for (F16Engine engine : engines) {
-            engine.start();
-        }
+//         List<F16Engine> engines = new ArrayList<>();
+//         engines.add(f16Engine);
+//         for (F16Engine engine : engines) {
+//             engine.start();
+//         }
+//     }
+// }
+
+public interface IEngine {
+
+    void start();
+}
+
+public class F16Engine implements IEngine {
+
+    @Override
+    public void start() {
+        System.out.println("F16 engine on");
     }
 }
