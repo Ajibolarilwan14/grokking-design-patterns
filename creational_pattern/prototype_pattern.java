d2944bb824f9fecc94d0007db9ff4bcb377d93f2
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2022-02-04 11:03:55 +0100

    created an F16 class that implements prototype interface

--- creational_pattern/prototype_pattern.java
+++ creational_pattern/prototype_pattern.java
@@ -9,3 +9,23 @@ public interface prototype_pattern {
 
     void setEngine(F16Engine f16Engine);
 }
+
+public class F16 implements prototype_pattern {
+    // default engine
+    F16Engine f16Engine = new F16Engine();
+
+    @Override
+    public void fly() {
+        System.out.println("F-16 flying...");
+    }
+
+    @Override
+    public IAircraftPrototype clone() {
+        // Deep clone self and return the product
+        return new F16();
+    }
+
+    public void setEngine(F16Engine f16Engine) {
+        this.f16Engine = f16Engine;
+    }
+}
