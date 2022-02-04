db390e801757e80ca3338aeafbf5944ea25f07d0
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2022-02-04 11:00:19 +0100

    implements interface for prototype pattern

---
+++ creational_pattern/prototype_pattern.java
@@ -0,0 +1,23 @@
+/**
+ * prototype_pattern
+ */
+public interface prototype_pattern {
+
+    // default engine
+    F16Engine f16Engine = new F16Engine();
+
+    @Override
+    public void fly() {
+        System.out.println("F-16 flying...");
+    }
+
+    @Override
+    public IAircraftPrototype clone() {
+        // Deep clone self and return the product
+        return new F16();
+    }
+
+    public void setEngine(F16Engine f16Engine) {
+        this.f16Engine = f16Engine;
+    }
+}
