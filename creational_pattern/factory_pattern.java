db703e00ec0e07e0f68b0b340cbcd656cc82e64a
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2022-02-04 11:38:28 +0100

    added the client class to consume it

--- creational_pattern/factory_pattern.java
+++ creational_pattern/factory_pattern.java
@@ -86,3 +86,17 @@ public class F16B extends F16 {
         return this;
     }
 }
+
+public class Client {
+    public void main() {
+        Collection<F16> myAirForce = new ArrayList<F16>();
+        F16 f16A = new F16A();
+        F16 f16B = new F16B();
+        myAirForce.add(f16A);
+        myAirForce.add(f16B);
+
+        for (F16 f16 : myAirForce) {
+            f16.fly();
+        }
+    }
+}
